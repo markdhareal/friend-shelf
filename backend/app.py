57dc052737a326bac07942ec96f20794a71c9a0b
Dharyl <programmingdha@gmail.com>
2024-11-09 05:36:08 +0800

    Created a Friend Model in models.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# CONFIGURATIONS
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#DATABASE
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return '<h1>Hello, World</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)

6e662aaae08615129fce83ad2aa3ea2928e16ca5
Dharyl <programmingdha@gmail.com>
2024-11-11 07:02:45 +0800

    Created Some Components

--- frontend/src/components/CreateUser.jsx
+++ frontend/src/components/CreateUser.jsx
@@ -1,7 +1,63 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { BiAddToQueue } from "react-icons/bi";
 import React from "react";
 
 const CreateUser = () => {
-  return <div>CreateUser</div>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>
+        <BiAddToQueue size={20} />
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+
+        <ModalContent>
+          <ModalHeader>Friend Info</ModalHeader>
+
+          <ModalCloseButton />
+
+          <ModalBody pb={6}>
+            <Flex alignItems={"center"} gap={4}>
+              <FormControl>
+                <FormLabel>Full Name</FormLabel>
+                <Input placeholder="Name" />
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Role</FormLabel>
+                <Input placeholder="Data Scientist" />
+              </FormControl>
+            </Flex>
+
+            <FormControl mt={4}>
+              <FormLabel>Description</FormLabel>
+              <Textarea
+                resize={"none"}
+                overflowY={"hidden"}
+                placeholder="He/She is a Data Scientist"
+              />
+            </FormControl>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 };
 
 export default CreateUser;
